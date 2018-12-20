f66b2d6e101677cdb3b7898c66bb79020b7d8151
Egor Shustov <egorshustov.93@gmail.com>
2018-12-21 00:07:14 +0500

    AsyncTasks replaced by Coroutines

--- app/src/main/java/com/egorshustov/mvvmtest/data/source/NotesRepository.kt
+++ app/src/main/java/com/egorshustov/mvvmtest/data/source/NotesRepository.kt
@@ -1,11 +1,12 @@
 package com.egorshustov.mvvmtest.data.source
 
-import android.os.AsyncTask
 import androidx.lifecycle.LiveData
 import android.app.Application
 import com.egorshustov.mvvmtest.data.source.local.AppDatabase
 import com.egorshustov.mvvmtest.data.source.local.NotesDao
 import com.egorshustov.mvvmtest.data.Note
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class NotesRepository(application: Application) {
@@ -21,68 +22,30 @@ class NotesRepository(application: Application) {
     // These methods are the API that NotesRepository exposes
     // to the outside:
     fun insert(note: Note) {
-        InsertNoteAsyncTask(notesDao).execute(note)
+        GlobalScope.launch {
+            notesDao.insert(note)
+        }
     }
 
     fun update(note: Note) {
-        UpdateNoteAsyncTask(notesDao).execute(note)
+        GlobalScope.launch {
+            notesDao.update(note)
+        }
     }
 
     fun delete(note: Note) {
-        DeleteNoteAsyncTask(notesDao).execute(note)
+        GlobalScope.launch {
+            notesDao.delete(note)
+        }
     }
 
     fun deleteAllNotes() {
-        DeleteAllNotesAsyncTask(notesDao).execute()
+        GlobalScope.launch {
+            notesDao.deleteAllNotes()
+        }
     }
 
     fun getAllNotes(): LiveData<List<Note>> {
         return allNotes
-    }
-
-    // AsyncTasks has to be static companion objects
-    // (must not have a reference to repository itself,
-    // otherwise this could cause a memory leak):
-    companion object {
-
-        private class InsertNoteAsyncTask
-        internal constructor(private val notesDao: NotesDao) :
-            AsyncTask<Note, Void, Void>() {
-
-            override fun doInBackground(vararg notes: Note): Void? {
-                notesDao.insert(notes[0])
-                return null
-            }
-        }
-
-        private class UpdateNoteAsyncTask
-        internal constructor(private val notesDao: NotesDao) :
-            AsyncTask<Note, Void, Void>() {
-
-            override fun doInBackground(vararg notes: Note): Void? {
-                notesDao.update(notes[0])
-                return null
-            }
-        }
-
-        private class DeleteNoteAsyncTask
-        internal constructor(private val notesDao: NotesDao) :
-            AsyncTask<Note, Void, Void>() {
-
-            override fun doInBackground(vararg notes: Note): Void? {
-                notesDao.delete(notes[0])
-                return null
-            }
-        }
-
-        private class DeleteAllNotesAsyncTask
-        internal constructor(private val notesDao: NotesDao) :
-            AsyncTask<Void, Void, Void>() {
-
-            override fun doInBackground(vararg voids: Void): Void? {
-                notesDao.deleteAllNotes()
-                return null
-            }
-        }
     }
 }
